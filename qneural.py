0a3cd25dde45702af56c2e6949c963d503ca7e8d
daniel <pinkdaniel@web.de>
2022-04-04 11:28:21 +0200

    remove duplicated code

--- qneural.py
+++ qneural.py
@@ -19,36 +19,6 @@ INPUT_SIZE = 9
 OUTPUT_SIZE = 9
 
 
-class TicTacNet(nn.Module):
-    def __init__(self):
-        # super().__init__()
-        super(TicTacNet, self).__init__()
-        hidden_layer = 36  # int((INPUT_SIZE/3)*2)+OUTPUT_SIZE
-        self.net = nn.Sequential(
-            nn.Linear(INPUT_SIZE, hidden_layer),
-            nn.ReLU(),
-            nn.Linear(hidden_layer, hidden_layer),
-            nn.ReLU(),
-            nn.Linear(hidden_layer, OUTPUT_SIZE),
-            nn.Sigmoid()
-        )
-
-    def forward(self, input):
-        return self.net.forward(input)
-
-
-class NetContext:
-    def __init__(self, policy_net, target_net, optimizer, loss_function):
-        self.policy_net = policy_net
-
-        self.target_net = target_net
-        self.target_net.load_state_dict(self.policy_net.state_dict())
-        self.target_net = self.target_net.eval()
-
-        self.optimizer = optimizer
-        self.loss_function = loss_function
-
-
 def create_qneural_player(net_context):
     def play(board):
         model = net_context.target_net
